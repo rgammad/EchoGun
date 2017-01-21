9bc8b4b13eeafbdb0e6b1c497f6b6c93d32de191
Roldan Gammad <rgammad@uci.edu>
2017-01-20 21:50:37 -0800

    Added Laser Gun

---
+++ EchoGun/EchoGun/Assets/Scripts/EchoGun.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoGun : MonoBehaviour {
+
+    PlayerMovement playerMovement;
+    EchoGunAPI egAPI;
+    //enum for different types of weapons
+    public enum eGun
+    {
+        STANDARD
+    }
+
+    void Start()
+    {
+        egAPI = GetComponent<EchoGunAPI>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public eGun currentEchoType;    
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+
+    
+    private void Shoot()
+    {
+        switch (currentEchoType)
+        {
+            case eGun.STANDARD:
+                egAPI.echoStandard(playerMovement.rawAimingInput);
+                break;
+            default:
+                break;
+        }
+    }
+}

37da657b028e139a0395af0da94c718808296a4b
Hudson Hughes <96hudson@gmail.com>
2017-01-21 13:09:34 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	EchoGun/EchoGun/Assets/DevelopmentOnly/Derek.unity
    #	EchoGun/EchoGun/Assets/Images/muzzle-flash-1.png.meta

--- EchoGun/EchoGun/Assets/Scripts/EchoGun.cs
+++ EchoGun/EchoGun/Assets/Scripts/EchoGun.cs
@@ -5,64 +5,72 @@ using UnityEngine;
 [RequireComponent(typeof(EchoGunAPI))]
 public class EchoGun : MonoBehaviour {
 
-    PlayerMovement playerMovement;
-    EchoGunAPI egAPI;
-    public WeaponType weapType;
+	PlayerMovement playerMovement;
+	EchoGunAPI egAPI;
+	public WeaponType weapType;
 	GameObject muzzle;
+	GameObject flash1;
 
-    //enum for different types of weapons
-    public enum eGun
-    {
-        STANDARD,
-        PROJECTILE,
-    }
+	//enum for different types of weapons
+	public enum eGun
+	{
+		STANDARD,
+		PROJECTILE,
+	}
 
-    void Start()
-    {
-        egAPI = GetComponent<EchoGunAPI>();
-        playerMovement = GetComponent<PlayerMovement>();
+	void Start()
+	{
+		egAPI = GetComponent<EchoGunAPI>();
+		playerMovement = GetComponent<PlayerMovement>();
 		muzzle = GameObject.Find ("MuzzlePoint");
-    }
+		flash1 = muzzle.transform.FindChild ("muzzle-flash-1").gameObject;
+	}
 
-    public eGun currentEchoType;    
+	public eGun currentEchoType;    
 
 	void Update()
-    {
-        
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Shoot();
-        }
-    }
+	{
 
-    
-    private void Shoot()
-    {
-        switch (currentEchoType)
-        {
-            case eGun.STANDARD:
-				muzzleFlash(eGun.STANDARD);
-                egAPI.echoStandard(weapType,playerMovement.rawAimingInput);
-                break;
-			case eGun.PROJECTILE:
-				muzzleFlash(eGun.PROJECTILE);
-                egAPI.echoProjectile(weapType,playerMovement.rawAimingInput);
-                break;
-            default:
-                break;
-        }
-    }
-	private void muzzleFlash(eGun type){
-		switch (type)
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+	}
+
+
+	private void Shoot()
+	{
+		switch (currentEchoType)
 		{
 		case eGun.STANDARD:
-			
+			muzzleFlash(eGun.STANDARD);
+			egAPI.echoStandard(weapType,playerMovement.rawAimingInput);
 			break;
 		case eGun.PROJECTILE:
-			
+			muzzleFlash(eGun.PROJECTILE);
+			egAPI.echoProjectile(weapType,playerMovement.rawAimingInput);
 			break;
 		default:
 			break;
 		}
+	}
+	private void muzzleFlash(eGun type){
+		switch (type)
+		{
+		case eGun.STANDARD:
+
+			flash1.SetActive (true);
+			Invoke ("disableMuzzles", 0.1f);
+			break;
+		case eGun.PROJECTILE:
+
+			break;
+		default:
+			break;
+		}
+	}
+
+	void disableMuzzles(){
+		flash1.SetActive (false);
 	}
 }

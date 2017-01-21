874b1d853fd34a8543a5a530c70d27ddf035f3e0
Reddeyfish <edrichd@uci.edu>
2017-01-20 19:50:35 -0800

    Create Unity Project

---
+++ EchoGun/EchoGun/Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}

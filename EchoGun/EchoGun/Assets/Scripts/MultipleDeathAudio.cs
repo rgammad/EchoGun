f645636d6051ce1f9691e94e304f8186148d69bf
Zachary Hart <zhhart@uci.edu>
2017-01-22 13:34:29 -0800

    Made tweaks to the storm trooper death prefab

---
+++ EchoGun/EchoGun/Assets/Scripts/MultipleDeathAudio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MultipleDeathAudio : MonoBehaviour, ISpawnable {
+
+    AudioSource sound;
+
+    [SerializeField]
+    protected AudioClip[] deathSounds;
+
+    [SerializeField]
+    protected int lengthOfArray;
+
+    void ISpawnable.Spawn()
+    {
+        int randomInt = UnityEngine.Random.Range(0, lengthOfArray);
+        sound.PlayOneShot(deathSounds[randomInt]);
+        Callback.FireAndForget(() => SimplePool.Despawn(this.transform.root.gameObject), deathSounds[randomInt].length, this);
+    }
+
+    // Use this for initialization
+    void Awake () {
+        sound = GetComponent<AudioSource>();
+
+    }
+}

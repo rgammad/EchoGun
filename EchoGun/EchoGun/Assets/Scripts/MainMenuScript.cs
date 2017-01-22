a67caa9cb46ddf5ab8f047181e47565eff051278
natalie42 <natalie42@me.com>
2017-01-22 14:18:14 -0800

    Controls Menu added to Main

--- EchoGun/EchoGun/Assets/Scripts/MainMenuScript.cs
+++ EchoGun/EchoGun/Assets/Scripts/MainMenuScript.cs
@@ -16,6 +16,11 @@ public class MainMenuScript : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+	public void ControlsButton()
+	{
+		SceneManager.LoadScene (3);
+	}
+
     public void BackToMainMenu()
     {
         SceneManager.LoadScene(0);

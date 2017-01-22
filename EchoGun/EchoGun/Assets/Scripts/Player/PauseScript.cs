e9477a882c484535274a11016acafbd7e6403db6
Hudson Hughes <96hudson@gmail.com>
2017-01-21 21:32:58 -0800

    Pause Menu

---
+++ EchoGun/EchoGun/Assets/Scripts/Player/PauseScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+	bool isPause = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPause = !isPause;
+				if(isPause)
+					Time.timeScale = 0;
+				else
+					Time.timeScale = 1;
+		}
+	}
+
+	void onGUI() {
+		if (isPause) {
+			GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
+			centeredStyle.alignment = TextAnchor.UpperCenter;
+			GUI.Label (new Rect (Screen.width/2-50, Screen.height/2-25, 100, 50), "BLAH", centeredStyle);
+		}
+	}
+}

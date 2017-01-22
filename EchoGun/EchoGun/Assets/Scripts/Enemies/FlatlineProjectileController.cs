ba30fe48a255c2cd73303dd996ce8c3510947baa
Zachary Hart <zhhart@uci.edu>
2017-01-22 12:46:57 -0800

    Made some changes to the projectile prefabs

--- EchoGun/EchoGun/Assets/Scripts/Enemies/FlatlineProjectileController.cs
+++ EchoGun/EchoGun/Assets/Scripts/Enemies/FlatlineProjectileController.cs
@@ -6,14 +6,22 @@ public class FlatlineProjectileController : MonoBehaviour, ISpawnable {
     public int projSpeed = 10;
     public float projDamage = 10f;
 
+    [SerializeField]
+    protected GameObject deathEffectWall;
+
+    [SerializeField]
+    protected GameObject deathEffectFlesh;
+
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Wall")) {
             SimplePool.Despawn(this.gameObject);
+            SimplePool.Spawn(deathEffectWall);
         }
         else if (other.gameObject.CompareTag("Player")) {
             other.transform.root.GetComponent<Health>().Damage(projDamage);
             Camera.main.GetComponent<CameraShakeScript>().screenShake(0.5f);
             SimplePool.Despawn(this.gameObject);
+            SimplePool.Spawn(deathEffectFlesh);
         }
     }
 
--- EchoGun/EchoGun/Assets/Scripts/EnemyProjectileController.cs
+++ EchoGun/EchoGun/Assets/Scripts/EnemyProjectileController.cs
@@ -10,13 +10,22 @@ public class EnemyProjectileController : MonoBehaviour, ISpawnable {
     [SerializeField]
     protected float projDamage = 10f;
 
+    [SerializeField]
+    protected GameObject deathEffectWall;
+
+    [SerializeField]
+    protected GameObject deathEffectFlesh;
+
+
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Wall")) {
             SimplePool.Despawn(this.gameObject);
+            SimplePool.Spawn(deathEffectWall);
         }
         if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Destructible")) {
             other.transform.root.GetComponent<Health>().Damage(projDamage);
             SimplePool.Despawn(this.gameObject);
+            SimplePool.Spawn(deathEffectFlesh);
         }
     }
 

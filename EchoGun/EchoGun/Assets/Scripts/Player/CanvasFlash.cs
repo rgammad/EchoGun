d7d38ce2bdaf2db56ee6442fd0868236ceff77ee
Reddeyfish <edrichd@uci.edu>
2017-01-21 23:17:59 -0800

    Add death screen

---
+++ EchoGun/EchoGun/Assets/Scripts/Player/CanvasFlash.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Text))]
+public class CanvasFlash : MonoBehaviour {
+
+    Text text;
+
+    [SerializeField]
+    protected Gradient colorGradient;
+
+    [SerializeField]
+    protected float flashDuration;
+
+    void Start () {
+        text = GetComponent<Text>();
+    }
+
+    public void Flash() {
+        Callback.DoLerp((float l) => {
+            text.color = colorGradient.Evaluate(l);
+        }, flashDuration, this);
+    }
+}

88beda2b79e13c5956e3520d23a04b96532cacc5
Reddeyfish <edrichd@uci.edu>
2017-01-21 19:27:52 -0800

    Add enemy layers and bullets

---
+++ EchoGun/EchoGun/Assets/Scripts/EnemyProjectileController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyProjectileController : MonoBehaviour, ISpawnable {
+
+    [SerializeField]
+    protected int projSpeed = 20;
+    [SerializeField]
+    protected float projDamage = 10f;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Wall")) {
+            SimplePool.Despawn(this.gameObject);
+        }
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Destructible")) {
+            other.transform.root.GetComponent<Health>().Damage(projDamage);
+            SimplePool.Despawn(this.gameObject);
+        }
+    }
+
+    void ISpawnable.Spawn() {
+        GetComponent<Rigidbody2D>().velocity = transform.right * projSpeed;
+    }
+}
